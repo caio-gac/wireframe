59ca137a7bca5169ae8610dac5368bf7928a8027
caio-gac <caio.correia@aluno.unc.br>
2022-05-31 11:17:25 -0300

    Calculo de idade e começo validador

---
+++ js/age.js
@@ -0,0 +1,12 @@
+function ageCalculate(){
+    var yearInput;
+    var monthInput;
+    var dayInput;
+    var input = yearInput +" - "+monthInput+" - "+dayInput;
+    var dateToday = new Date(input);
+    var monthDiff = Date.now() - dateToday.getTime();
+    var ageToDate = new Date(monthDiff);    
+    var year = ageToDate.getUTCFullYear();
+    var age = Math.abs(year - 1970);
+    return document.getElementById('ageIndex').innerHTML = "age"
+}
