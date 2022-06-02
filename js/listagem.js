1330228376df2e737b7d4c898fe8dcff8ed1d346
Heroldi <heroldi.candido@aluno.unc.br>
2022-06-02 13:47:55 -0300

    Recriando listagem

--- js/listagem.js
+++ js/listagem.js
@@ -1,64 +1,45 @@
-// var arr = [];
+var user1 = [1,2,3,4,5,6,7,8,9,null,2,null,null,14,15,16];
 
-// arr = {nome:"heroldi", nick:"nick1", age:"20"};
-
-
-// // var a = JSON.parse(arr);
-
-// var a = localStorage.getItem('dado')
-// console.log(a);
+localStorage.setItem("user",JSON.stringify(user1));
 
 
-var a = "nome1";
-var b = "nick1";
-var c = 20;
+var labelData = document.querySelector(".showContainer");
+var localStorageData = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null];
+var labelInput = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null];
+var labelInputContent = ["Name: ", "Nickname: ", "Email", "Phone", "Birthday", "Age", "Linkdin", "Github", "1º Certificate", "2º Certificate", "3º Certificate", "4º Certificate", "5º Certificate","Team Name", "Institution", "Graduation"]
 
-var tableTr = document.querySelector(".dataTr");
+var data = JSON.parse(localStorage.getItem('user'));
 
-var nametd = document.createElement("td");
-var nicktd = document.createElement("td");
-var agetd = document.createElement("td");
-var optiontd = document.createElement("td");
-
-nametd.textContent = a;
-nicktd.textContent = b;
-agetd.textContent = c;
-optiontd.textContent = "more";
-
-tableTr.appendChild(nametd);
-tableTr.appendChild(nicktd);
-tableTr.appendChild(agetd);
-tableTr.appendChild(optiontd);
-
-
-nametd.classList.add("bodyTable");
-nicktd.classList.add("bodyTable");
-agetd.classList.add("bodyTable");
-optiontd.setAttribute("class", "optionRef");
-optiontd.classList.add("bodyTable");
-
-
-function iniciaModal(modalID) {
-    const modal = document.getElementById(modalID);
-    if(modal) {
-        modal.classList.add('mostrar');
-        modal.addEventListener('click', (e) => {
-            if(e.target.id == modalID || e.target.className == 'fecharModal') {
-                modal.classList.remove('mostrar');
-             }
-        });
-    }
+for(let x = 0; x<localStorageData.length; x++){
+    localStorageData[x] = data[x];
 }
 
-const logo = document.querySelector('.optionRef');
-logo.addEventListener('click', () => iniciaModal('modalIdent'));
 
-document.addEventListener('scroll', () => {
-    if(window.pageYOffset > 800) {
-        iniciaModal('modalIdent')
+var campLabel = [1];
+campLabel.length = localStorageData.length;
+var border = document.createElement("div");
+border.setAttribute("class", "boderInput")
+
+for(let i=0; i<labelInput.length;i++){
+    if(localStorageData[i] != null){
+        labelInput[i] = document.createElement("label");
+        campLabel[i] = document.createElement("label");      
+
+        labelInput[i].textContent = labelInputContent[i];
+        campLabel[i].textContent = localStorageData[i];
+
+        labelData.appendChild(border);
+        border.appendChild(labelInput[i]);
+        labelData.appendChild(border);
+        border.appendChild(campLabel[i]);  
+
+        labelInput[i].classList.add("labelInput");
+        campLabel[i].classList.add("label");      
+
+    }else{  
+
     }
-})
-
+};
 
 
 
