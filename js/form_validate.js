b91d0655523a6595c8135a73290d48323e6a7ff3
caio-gac <caio.correia@aluno.unc.br>
2022-06-02 09:09:18 -0300

    Mudancas do dia 01/06

--- js/form_validate.js
+++ js/form_validate.js
@@ -1,5 +1,8 @@
 function validate(){
     yesyes = 0
+    inputName = document.getElementById('name');
+    inputEmail = document.getElementById('email');
+    inputBirthday = document.getElementById('birth');
     x = document.getElementById(yesyes).value;
     if(x==null || x==""){
         alert('Por favor preencha o campo '+yesyes+'');
--- js/storage.js
+++ js/storage.js
@@ -1,7 +1,14 @@
+let input1 = document.querySelector('#1'); 
+
 function store(item_ids) {
+    let input1Value = input1.value;
     var items = [];
+    localStorage.setItem('ids', items);
     for(var i = 0; i < item_ids.length; i++) {
-        items.push(item_ids[i]);
+        items.push(`input${i}Value`);
+    }
+    if (items !== []) {
+        localStorage.getItem('ids');
     }
     localStorage.setItem("item", JSON.stringify(items));
 }
