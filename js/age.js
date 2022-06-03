2f5caa8a0599e63a2da9c879e167f01ee10aa281
caio-gac <caio.correia@aluno.unc.br>
2022-06-03 17:00:14 -0300

    upload

--- js/age.js
+++ js/age.js
@@ -1,7 +1,7 @@
 function ageCalculate(){
-    var yearInput;
-    var monthInput;
-    var dayInput;
+    var yearInput = document.getElementById('year');
+    var monthInput = document.getElementById('month');;
+    var dayInput = document.getElementById('day');;
     var input = yearInput +" / "+monthInput+" / "+dayInput;
     var dateToday = new Date(input);
     var monthDiff = Date.now() - dateToday.getTime();
