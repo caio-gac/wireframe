88d27e18e1fdc21522a6b39ec891b6893d16de8a
caio-gac <caio.correia@aluno.unc.br>
2022-06-07 14:52:01 -0300

    phon change 1

--- js/phone_validate.js
+++ js/phone_validate.js
@@ -1,14 +1,24 @@
-const input = document.querySelector('.inp-phone')
-input.addEventListener('input', handleInput, false)
+// const $input = document.querySelector('.inp-phone')
+// $input.addEventListener('input', handleInput, false)
 
-function handleInput (e) {
-  e.target.value = phoneMask(e.target.value)
-}
+// function handleInput (e) {
+//   e.target.value = phoneMask(e.target.value)
+// }
 
-function phoneMask (phone) {
-  return phone.replace(/\D/g, '')
-    .replace(/^(\d)/, '($1')
-    .replace(/^(\(\d{2})(\d)/, '$1) $2')
-    .replace(/(\d{4})(\d{1,5})/, '$1-$2')
-    .replace(/(-\d{5})\d+?$/, '$1');
+
+function phoneMask () {
+  phoneNum = document.querySelector('.inp-phone').value ;
+  phoneReg = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  if (phone.value.match(phoneReg)){
+    return true;
+  }else{
+    alert('Phone error')
+    return false
+  }
+
+  // phone.replace(/\D/g, '')
+  //   .replace(/^(\d)/, '($1')
+  //   .replace(/^(\(\d{2})(\d)/, '$1) $2')
+  //   .replace(/(\d{4})(\d{1,5})/, '$1-$2')
+  //   .replace(/(-\d{5})\d+?$/, '$1');
 }
